82205eaccd94bd2b0f1873361fe31afb0d9e9b9e
LapsTime <39061148+LapsTimeOFF@users.noreply.github.com>
2022-04-17 17:03:00 +0200

    EDIT

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 "use strict";
 const nodemailer = require('nodemailer')
 const config = require('./config.json')
+const axios = require('axios')
+const JSON5 = require('json5')
 const log = console.log;
+
+function htmlForm(res) {
+    let final = ""
+
+    for (let _i = 0; _i < res.length; _i++) {
+        const risk = res[_i];
+        
+        final = final + `<br><br><u>${risk.pollenName} :</u><br>    Niveau : ${risk.level}`
+    }
+}
 
 async function main() { // J'utilise async pour le sendMail
     const transporter = nodemailer.createTransport({
@@ -10,14 +22,25 @@ async function main() { // J'utilise async pour le sendMail
         secure: config.smtp.secure, 
         auth: config.smtp.auth
     })
+    
+    let result = ""
+
+    await axios
+        .get(`https://pollens.fr/risks/thea/counties/${config.countyNumber}`)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            console.log(res.data)
+            result = res.data
+        })
+    
+    log(result)
 
     await transporter.sendMail({
         from: '"Pollen Info" <'+config.smtp.auth.user+'>', // J'utilise la config pour éviter de leak mon mail perso :|
         to: config.receivers, // Le spam... C'EST MAL. et c'est chiant.
         subject: "Bilan Pollen",
-        html: "<b>Test email</b>"
+        html: `<b>Bonjour,</b><br><br><p>Voici le bilan d'alerte pollen pour le département <b>${result.countyNumber} - ${result.countyName}</b> :<br>Niveau d'alert du département : ${result.riskLevel}</p><br><br>${htmlForm(result.risks)}`
     })
-
     log('Email envoyé');
 }
 
