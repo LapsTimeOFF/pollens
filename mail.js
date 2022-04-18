c621b073a42aa22db18c945a8c0a0b0fdf432cfb
LapsTime <39061148+LapsTimeOFF@users.noreply.github.com>
2022-04-18 15:58:14 +0200

    YESS

--- mail.js
+++ mail.js
@@ -42,4 +42,26 @@ async function sendMail() { // J'utilise async pour le sendMail
     log('Email envoyé');
 }
 
-module.exports = sendMail;
+module.exports.sendMail = async function () { // J'utilise async pour le sendMail
+    log('sendMail on ' + __filename + ' started')
+    const transporter = nodemailer.createTransport(config.smtp)
+    
+    let result = ""
+
+    await axios
+        .get(`https://pollens.fr/risks/thea/counties/${config.countyNumber}`)
+        .then(res => {
+            console.log(`statusCode: ${res.status}`)
+            result = res.data
+        })
+
+    color = config.colors[result.riskLevel]
+
+    await transporter.sendMail({
+        from: '"Pollen Info" <'+config.smtp.auth.user+'>', // J'utilise la config pour éviter de leak mon mail perso :|
+        to: config.receivers, // Le spam... C'EST MAL. et c'est chiant.
+        subject: "Bilan Pollen",
+        html: `<b>Bonjour,</b><br><br><p>Voici le bilan d'alerte pollen pour le département <b>${result.countyNumber} - ${result.countyName}</b> :<br>Niveau d'alerte du département :</p> <p style="color: ${color};">${result.riskLevel}</p>${htmlForm(result.risks)}`
+    })
+    log('Email envoyé');
+};
