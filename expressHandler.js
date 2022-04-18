886eb5f76f7656d69e3f704e7b82a48e9777e484
LapsTime <39061148+LapsTimeOFF@users.noreply.github.com>
2022-04-18 15:46:27 +0200

    sendMail

--- expressHandler.js
+++ expressHandler.js
@@ -4,3 +4,11 @@ const mail = require('./mail')
 module.exports.sendIndex = function (_, res) {
     res.sendFile(`${__dirname}/public/index.html`)
 }
+
+module.exports.sendMail = function (_, res) {
+    mail.sendMail()
+
+    res.send({
+        "statusCode": 200
+    })
+}
--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const app = express();
 
 async function main() {
     createLink('/', expressHandler.sendIndex)
+    createLink('/api/v1/mail', expressHandler.sendIndex)
     startExpress(config.api.port)
 }
 
 async function createLink(url, handler) {
-    if(url === undefined || handler === undefined) log('No url or handler defined for createLink()')
+    if(url === undefined || handler === undefined){ log('No url or handler defined for createLink()'); return }
     app.get(url, (req, res) => {
         handler(req, res)
     })
