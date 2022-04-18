b2dcf2d768ba4f0668e1299d1285e646b6564c1a
LapsTime <39061148+LapsTimeOFF@users.noreply.github.com>
2022-04-18 15:35:47 +0200

    import expressHandler and error if undefined

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const config = require('./config.json')
 
+const expressHandler = require('./expressHandler')
+
 const express = require('express')
 const log = console.log;
 
 const app = express();
 
 async function main() {
-    createLink()
+    createLink('')
     startExpress(config.api.port)
 }
 
 async function createLink(url, handler) {
+    if(url === undefined || handler === undefined) log('No url or handler defined for createLink()')
     app.get(url, (req, res) => {
         handler(req, res)
     })
